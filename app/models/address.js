361efc88765895def9e6360ba6438ef07731ca91
Patrick Mulder <mulder.patrick@gmail.com>
2014-03-24 23:25:27 +0100

    rm firebase for model

--- app/models/address.js
+++ app/models/address.js
@@ -2,8 +2,6 @@ var Backbone = require("backbone");
 
 var Address = Backbone.Firebase.Model.extend({
 
-  firebase: "https://addresses-demo.firebaseio.com/addresses",
-
   defaults: {
     "name": "empty",
     "email": "empty"
