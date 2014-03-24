571ba129dc504210a2f17378dc535cbddc8064ef
Patrick Mulder <mulder.patrick@gmail.com>
2014-03-24 23:25:46 +0100

    compile bundle

--- static/bundle.js
+++ static/bundle.js
@@ -55,8 +55,6 @@ var Backbone = require("backbone");
 
 var Address = Backbone.Firebase.Model.extend({
 
-  firebase: "https://addresses-demo.firebaseio.com/addresses",
-
   defaults: {
     "name": "empty",
     "email": "empty"
